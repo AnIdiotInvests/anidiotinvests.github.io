0e9bcda896cea86969ec7adee9c49862c37a288b
blah <blah@blah.com>
2025-05-20 20:33:03 -0500

    navigation rework

--- js/component-loader.js
+++ js/component-loader.js
@@ -6,9 +6,32 @@ async function loadComponents() {
     for (const [key, value] of components) {
         document.getElementById(key).innerHTML = await fetchHtml(value)
     }
+
+    setNav();
 }
 
 async function fetchHtml(loc) {
     response = await fetch(loc);
     return await response.text();
+}
+
+async function setNav() {
+    const navEle = document.getElementById('get-nav');
+    if (navEle) {
+        navEle.classList.remove('active');
+        const url = location.pathname;
+        if (url) {
+            if (url.trim() == "/") {
+                applyClass('home')
+
+            } else if (url.trim() === '/about.html') {
+                applyClass('about')
+            }
+        }
+    }
+}
+
+async function applyClass(id) {
+    aboutEle = document.getElementById(id);
+    aboutEle.classList.add('active');
 }
